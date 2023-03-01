c15eef10b397772ab9f63349dcf3545ea6da3922
elmergustavo <elmergustavo79@gmail.com>
2023-02-28 18:10:32 -0600

    notification add in database

--- app/Notifications/SendNotifiEmail.php
+++ app/Notifications/SendNotifiEmail.php
@@ -9,19 +9,22 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 use Illuminate\Notifications\Messages\MailMessag;
+use Illuminate\Support\Facades\Log;
 
 class SendNotifiEmail extends Notification implements ShouldQueue
 {
     use Queueable;
+
+    public $product;
    
-    public function __construct(Product $product)
+    public function __construct($productos)
     {
-        
+         $this->product = $productos;
     }
 
     public function via()
     {
-        return ['mail'];
+        return ['mail', 'database'];
     }
 
     /**
@@ -31,14 +34,22 @@ class SendNotifiEmail extends Notification implements ShouldQueue
 
         return (new MailMessage)
             ->greeting('Hello!')
-            ->line('One of your invoices has been paid!')
-            ->line('Producto creado exitosamente!: ');
+            ->line('Nombre del Producto: '.  $this->product->name)
+            ->line('descripcion del Producto: '.  $this->product->description)
+            ->line('Stock del Producto: '.  $this->product->stock)
+            ->line('Precio del Producto: '.  $this->product->price)
+            ->line('Producto creado exitosamente');
     }
 
     public function toArray(): array
     {
         return [
-            //
+            'id' => $this->product->id,
+            'user_id' => $this->product->user_id,
+            'name' => $this->product->name,
+            'description' => $this->product->description,
+            'price' => $this->product->price,
+            'stock' => $this->product->stock,
         ];
     }
 }
